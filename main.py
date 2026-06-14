0f26c242a22096033b98ffa12bef7aa634397b26
Delgado-dot <delgadocristopher350@gmail.com>
2026-06-13 21:47:53 -0500

    Actualiza main con Hud y resolucion  1920x1080

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pygame
 import sys
+from game.UI.hud import HUD
 
-pantalla_ancho = 800
-pantalla_alto = 600
+pantalla_ancho = 1920
+pantalla_alto = 1080    
 FPS = 60
 
 def main():
     pygame.init()
     pantalla = pygame.display.set_mode((pantalla_ancho, pantalla_alto))
-    pygame.display.set_caption( "-----")
+    pygame.display.set_caption("-----")
     reloj = pygame.time.Clock()
+    hud = HUD()
+    inicio_tiempo = pygame.time.get_ticks()
     
     corriendo = True
     while corriendo:
@@ -17,11 +20,13 @@ def main():
             if evento.type == pygame.QUIT:
                 corriendo = False
         
-        pantalla.fill((30,30,30))
+        pantalla.fill((30, 30, 30))
+        hud.draw(pantalla, 3, 0, inicio_tiempo)
         pygame.display.flip()
         reloj.tick(FPS)
         
     pygame.quit()
     sys.exit()
+
 if __name__ == "__main__":
     main()
